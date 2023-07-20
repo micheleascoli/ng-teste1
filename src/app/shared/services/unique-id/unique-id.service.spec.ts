b0e0c7afef7af6ef85a79042b85c4545e9df847f
Michele Ascoli <micheleascoli17@gmail.com>
2023-07-20 15:51:40 -0500

    adicionando mais testes

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,11 +1,44 @@
 import { UniqueIdService as UniqueIdService } from "./unique-id.service";
 
 describe(UniqueIdService.name, () => {
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
-    const id = service.generateUniqueIdWithPrefix('app');
+  let service: UniqueIdService = null;
+  beforeEach (() => {
+    service = new UniqueIdService();
+  });
 
-    expect(id).toContain('app-');
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
+      const id = service.generateUniqueIdWithPrefix('app');
+      expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate IDs when called multiple times`, () => {
+      // O Set ele não aceita valores duplicados.
+      const ids = new Set();
+      //Gerando 50 ids, se houver algum repetido não dará erro.
+      //Mas o Set não vai adicionar.
+      for (let i = 0; i < 50; i++) {
+       ids.add(service.generateUniqueIdWithPrefix('app'));
+      }
+      // Agora só verifica se o tamanho do Set é igual a 50.
+      // se for menor é porque houve valores duplicados.
+      expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generatedIds when called`, () => {
+      service.generateUniqueIdWithPrefix('app');
+      service.generateUniqueIdWithPrefix('app');
+      expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should throw when called with empty`, () => {
+      const emptyValues = [null, undefined, '', '0', '1'];
+      emptyValues.forEach(emptyValue => {
+        expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow();
+      })
   });
 
 });
