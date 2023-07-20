f2cc2fb9e68909467a53458c2999ed61f77b729f
Michele Ascoli <micheleascoli17@gmail.com>
2023-07-20 14:37:39 -0500

    arquivo de teste com a estrutura base.

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,13 @@
+import { UniqueIdService as UniqueIdService } from "./unique-id.service";
+
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    const service = new UniqueIdService();
+    const id = service.generateUniqueIdWithPrefix('app');
+
+    expect(id).toContain('app-');
+  });
+
+});
+
+
