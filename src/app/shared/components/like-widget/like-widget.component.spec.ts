0c996c9c314b04af8effbda950a338e6b0dc3dd7
Michele Ascoli <micheleascoli17@gmail.com>
2023-07-21 15:56:55 -0500

    Criando um instância do componente

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,22 +1,33 @@
-import { TestBed } from "@angular/core/testing";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { LikeWidgetComponent } from "./like-widget.component";
+import { Component } from "@angular/compiler/src/core";
+import { UniqueIdService } from "../../services/unique-id/unique-id.service";
 
 describe(LikeWidgetComponent.name, () => {
-  let component: LikeWidgetComponent = null;
-/*
+  /**
+   * Fixture é tudo aquilo criado para te permitir testar
+   * o componente.
+   * Wrapper é um cara que embrulha o componente que foi criado e te dá um monte
+   *  de funções, um monte de métodos utilitários que você vai usar durante o teste.
+   */
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
+
   beforeEach(async () =>{
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent]
+      declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule]
     }).compileComponents();
-  });
-*/
 
-beforeEach(() =>{
-  TestBed.configureTestingModule({
-    declarations: [LikeWidgetComponent]
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   });
-});
-  it('', () => {
+
+  //Vai testar se no momento da fixture ser criada, não houve nenhum
+  //problema para criação da instância do componente.
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
 
   })
 
