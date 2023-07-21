4bcdddbb6a736cfdab907830724504aa34c2258b
Michele Ascoli <micheleascoli17@gmail.com>
2023-07-21 15:34:57 -0500

    Sobre TestBed

---
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from "@angular/core/testing";
+import { LikeWidgetComponent } from "./like-widget.component";
+
+describe(LikeWidgetComponent.name, () => {
+  let component: LikeWidgetComponent = null;
+/*
+  beforeEach(async () =>{
+    await TestBed.configureTestingModule({
+      declarations: [LikeWidgetComponent]
+    }).compileComponents();
+  });
+*/
+
+beforeEach(() =>{
+  TestBed.configureTestingModule({
+    declarations: [LikeWidgetComponent]
+  });
+});
+  it('', () => {
+
+  })
+
+});
--- src/app/shared/components/like-widget/like-widget.module.ts
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { LikeWidgetComponent } from "./like-widget.component";
+import { UniqueIdService } from "../../services/unique-id/unique-id.service";
 
 @NgModule({
   declarations: [LikeWidgetComponent],
@@ -9,6 +10,7 @@ import { LikeWidgetComponent } from "./like-widget.component";
     CommonModule,
     FontAwesomeModule,
   ],
-  exports: [LikeWidgetComponent]
+  exports: [LikeWidgetComponent],
+  providers: [UniqueIdService]
 })
 export class LikeWidgetModule {}
